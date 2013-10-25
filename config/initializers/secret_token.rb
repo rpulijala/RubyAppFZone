e156aa87a8d32e7063dad664cd142a13cec2803c
Raghuram Pulijala <raghuram.pulijala@gmail.com>
2013-10-25 09:25:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fzone::Application.config.secret_key_base = '6611d85dce26a7cf8029bb57a55ad2f84c32442f72138208e32f80c39c31243f82773144d594907b9e612f7247e8f68f2dd43b66f42f48af85df5979b3242166'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fzone::Application.config.session_store :cookie_store, key: '_fzone_session'
